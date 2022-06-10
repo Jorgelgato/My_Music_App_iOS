85867afbe67093d732cea6206b906f9bc464f763
Jorge Bobrek <jorge.bobrek@pragma.com.co>
2022-06-10 13:10:17 -0500

    Initial Commit

---
+++ MyMusicApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyMusicApp
+//
+//  Created by Jorge Bobrek on 10/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyMusicApp/MyMusicAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMusicAppApp.swift
+//  MyMusicApp
+//
+//  Created by Jorge Bobrek on 10/06/22.
+//
+
+import SwiftUI
+
+@main
+struct MyMusicAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
